013abd001b1f6a13f9b32ff91a70f88ab172ea25
NonsoAmadi10 <nonsoamadi@aol.com>
2021-09-11 23:54:16 +0100

    :hammer: finish lightning module

--- lib/bitnob/objects/lightning.rb
+++ lib/bitnob/objects/lightning.rb
@@ -15,5 +15,39 @@ class Lightning < Base
     post_request("#{base_url}#{BASE_ENDPOINTS::LN}/createinvoice", payload)
   end
 
-  def pay_invoice(data); end
+  def pay_invoice(data)
+    base_url = bitnob_object.base_url
+    required_parameters = %w[request reference customerEmail description]
+
+    check_passed_parameters(required_parameters, data)
+    payload = data.to_json
+
+    post_request("#{base_url}#{BASE_ENDPOINTS::LN}/pay", payload)
+  end
+
+  def initiate_payment(request)
+    base_url = bitnob_object.base_url
+    body = { 'request' => request }
+    payload = data.to_json
+
+    response = post_request("#{base_url}#{BASE_ENDPOINTS::LN}/initiatepayment", payload)
+
+    return response
+
+    def decode_payment_request(request)
+      base_url = bitnob_object.base_url
+      body = { 'request' => request }
+      payload = data.to_json
+
+      post_request("#{base_url}#{BASE_ENDPOINTS::LN}/decodepaymentrequest", payload)
+    end
+
+    def get_invoice(invoice_id)
+      base_url = bitnob_object.base_url
+      body = { 'id' => invoice_id }
+      payload = data.to_json
+
+      post_request("#{base_url}#{BASE_ENDPOINTS::LN}/getinvoice", payload)
+    end
+  end
 end
