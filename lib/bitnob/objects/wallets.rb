c9dbe325e8379ca9ebaa165f7355fce08adecdb3
NonsoAmadi10 <nonsoamadi@aol.com>
2021-09-12 23:47:02 +0100

    :hammer: small adjustments

--- lib/bitnob/objects/wallets.rb
+++ lib/bitnob/objects/wallets.rb
@@ -4,18 +4,18 @@ require_relative 'base/base'
 require 'json'
 
 class Wallets < Base
-  def fetch_wallets() 
-    base_url = bitnob_object.base_url 
+  def fetch_wallets
+    base_url = bitnob_object.base_url
     get_request("#{base_url}#{BaseEndpoints::ONCHAIN}")
   end
 
   def fetch_all_transactions(page_number: 1, limit: 10)
-    base_url = bitnob_object.base_url 
+    base_url = bitnob_object.base_url
     get_request("#{base_url}#{BaseEndpoints::ONCHAIN}?order=ASC&page=#{page_number}&take=#{limit}")
   end
 
   def fetch_a_single_transaction(transaction_id)
-    base_url = bitnob_object.base_url 
+    base_url = bitnob_object.base_url
     get_request("#{base_url}#{BaseEndpoints::ONCHAIN}/#{transaction_id}")
   end
 end
